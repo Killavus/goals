c3f00256f839770c08540d651c8aad8cd6145750
Marcin Grzywaczewski <killavus@gmail.com>
2017-09-18 04:56:25 +0200

    Created file-based persistance layer for tracking context

---
+++ backend/src/infrastructure/db/tracking.js
@@ -0,0 +1,64 @@
+import fs_ from 'fs'
+import path from 'path'
+import Promise from 'bluebird'
+
+const fs = Promise.promisifyAll(fs_, { suffix: 'Promise' })
+
+export function trackingDb (storagePath, dbApi) {
+  const storageFile = (key) => path.resolve(
+    __dirname,
+    storagePath,
+    `${key}.data`
+  )
+
+  const wrapError = (msg) => (err) => {
+    throw new Error(msg + `: ${err.message}`)
+  }
+
+  const rethrow = (err) => { throw err }
+
+  const concatData = (newData) => (oldData) => oldData.concat(newData)
+
+  const rewriteData = (key) => (data) => {
+    return dbApi.openPromise(storageFile(key), 'w+')
+      .then(
+        (fd) => {
+          const writeStream = fs.createWriteStream(
+            null,
+            {
+              autoClose: true,
+              encoding: 'utf8',
+              fd
+            }
+          )
+
+          writeStream.end(JSON.stringify(data))
+          return data
+        },
+        wrapError(`Failed to open datastream ${key} for writing`)
+      )
+  }
+
+  const readData = (key) =>
+    dbApi.readFilePromise(storageFile(key), 'utf8')
+      .then(
+        JSON.parse,
+        wrapError(`Failed to load datapoints stream with key ${key}`)
+      )
+
+  return {
+    append (key, points) {
+      return readData(key)
+        .then(concatData(points), rethrow)
+        .then(rewriteData(key), rethrow)
+    },
+    load (key) {
+      return readData(key)
+    },
+    overwrite (key, points) {
+      return rewriteData(key)(points)
+    }
+  }
+}
+
+export default trackingDb('./data', fs)
